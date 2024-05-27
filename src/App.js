819d5a3b4286c88bcd7c6e055c151cac6da30bc2
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-05-27 23:56:33 +0700

    After decalring React Route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.compoents';
+import HomePage from './pages/homepage/homepage.components';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
+    <Router>
     <div>
-      <HomePage />
+    <Routes>
+      <Route exact path='/' element={<HomePage />}/>
+      <Route path='/hats' element={<HatsPage />}/>
+    </Routes>
     </div>
+    </Router>
   );
 }
 
