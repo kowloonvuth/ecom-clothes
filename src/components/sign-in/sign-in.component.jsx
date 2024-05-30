fbb84231565b21700dce79f5b84d800658ca4911
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-05-30 18:13:44 +0700

    made a change to the sign in button including email/password and styling sign in button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+
 import './sign-in.style.scss';
 
 class SignIn extends React.Component{
@@ -30,21 +34,23 @@ class SignIn extends React.Component{
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
-                <form onSubmmit={this.handleSubmit}>
-                    <input name='email' 
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput 
+                           name='email' 
                            type="email" 
                            value={this.state.email} 
-                           onChange = {this.handleChange}
+                           label= 'email'
+                           handleChange = {this.handleChange}
                            required />
-                    <label>Email</label>
-                    <input name='password' 
+                    <FormInput 
+                           name='password' 
                            type="password" 
-                           value={this.state.password}
-                           onChange = {this.handleChange}
+                           label='password'
+                           value={this.state.email}
+                           handleChange = {this.handleChange}
                            required />
-                    <label>Password</label>
 
-                    <input type="submit" value="Submit Form"/>
+                    <CustomButton type='submit'>SIGN IN</CustomButton>
                 </form>
             </div>
         );
