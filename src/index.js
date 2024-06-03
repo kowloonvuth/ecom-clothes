978acc9907651872d229e81a659dd4b6f8b2feea
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-06-03 19:36:29 +0700

    finished setting up step 1 and step 2 of redux inside react

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux'
+
 
 import './index.css';
 import App from './App';
 
+import store from './redux/store';
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
