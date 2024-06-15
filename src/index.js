ae41a77aeabca9c6c5d71ff604daf4f3548b34ce
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-06-15 17:59:10 +0700

    add a redux persist to prevent storing items in the checkout pages from being lost

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux'
-
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 import App from './App';
 
-import store from './redux/store';
+import {store, persistor } from './redux/store';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </React.StrictMode>
   </Provider>
 );
