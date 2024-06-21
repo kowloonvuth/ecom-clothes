6f93a3eea9051c651ac52362deb117cbd39e1a21
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-06-21 23:51:21 +0700

    adjust

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from "react-redux";
 import { createStructuredSelector } from 'reselect';
 
-import { selectCollections } from '../../redux/shop/shop.selector';
+import { selectCollectionForPreview} from '../../redux/shop/shop.selector';
 
 import './collection-overview.style.scss';
 
@@ -10,14 +10,14 @@ import CollectionPreview from '../preview-collection/collection-preview';
 
 const CollectionsOverview = ({ collections }) => (
     <div className='collections-overview'>
-        {collections.map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
+        {collections.map(collection => (
+            <CollectionPreview key={collection} {...collection} />
         ))}
     </div>
 );
 
 const mapStateProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionForPreview
 });
 
 export default connect(mapStateProps)(CollectionsOverview);
